1c102387bbb21973e7206f67d3397c9ec21e8770
Patrick Wawrzoszek <patrick.wawrzoszek@hotmail.com>
2020-01-15 23:00:30 -0500

    feat: Executing js in class and sending to correct message handler. Need to externalize JS source now.

--- jumboChallenge/ViewController.swift
+++ jumboChallenge/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKScriptMessageHandler {
+    @IBOutlet weak var webView: WKWebView!
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "jumbo" {
+            print(message.body)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        webView.configuration.userContentController.add(self, name: "jumbo")
+        
+        let url = Bundle.main.resourceURL!.absoluteURL
+        let html = url.appendingPathComponent("index.html")
+        
+        webView.loadFileURL(html, allowingReadAccessTo: url)
+        
+        let greetingScript = "window.webkit.messageHandlers.jumbo.postMessage('Hello World')"
+        webView.evaluateJavaScript(greetingScript, completionHandler: nil)
+        
     }
-
-
 }
 
